75919df20be052c08d2a0415e0b6fc8aa00a22e9
KatheLJ <katheloajar257@gmail.com>
2023-03-17 21:00:54 -0600

    Cambios

--- SoportePatitos/Controllers/HomeController.cs
+++ SoportePatitos/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SoportePatitosBD.Modelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -21,8 +22,102 @@ namespace SoportePatitos.Controllers
             return View();
         }
 
+        //Accion que muestra la pantalla de inicio de sesion al empleado
+        public ActionResult Login()
+        {
+            return View();
+        }
 
-        
+
+
+
+        //Accion que valide los datos y permite o deniega el ingreso al usuario
+        public ActionResult InicioSesion(Empleado pEmpleado)
+        {
+            if (ModelState.IsValid)
+            {
+                //Se le indica al sistema que utilice dicha entidad para conectarse a la BD
+                using (SoportePatitosEntities ContextoBD = new SoportePatitosEntities())
+                {
+                    var data = ContextoBD.Empleado.Where(a => a.Usuario.Equals(pEmpleado.Usuario) &&
+                    a.Contraseña.Equals(pEmpleado.Contraseña) && a.ID_perfil.Equals(1)).ToList();
+
+                    var data2 = ContextoBD.Empleado.Where(a => a.Usuario.Equals(pEmpleado.Usuario) &&
+                    a.Contraseña.Equals(pEmpleado.Contraseña) && a.ID_perfil.Equals(2));
+
+                    var data3 = ContextoBD.Empleado.Where(a => a.Usuario.Equals(pEmpleado.Usuario) &&
+                    a.Contraseña.Equals(pEmpleado.Contraseña) && a.ID_perfil.Equals(3)).ToList();
+
+                    //var data = ContextoBD.Empleado.Where(a => a.Usuario.Equals(pEmpleado.Usuario) &&
+                    //a.Contraseña.Equals(pEmpleado.Contraseña) && a.Perfil.Equals(pEmpleado.Perfil)).ToList();
+
+                    /* var data2 = ContextoBD.Empleado.Where(a => a.Usuario.Equals(pEmpleado.Usuario) &&
+                     a.Contraseña.Equals(pEmpleado.Contraseña)).ToList();
+
+                     var data3 = ContextoBD.Empleado.Where(a => a.Usuario.Equals(pEmpleado.Usuario) &&
+                    a.Contraseña.Equals(pEmpleado.Contraseña)).ToList();*/
+
+
+                    Session["Gerencia"] = null;
+                    Session["RH"] = null;
+                    Session["Empleado"] = null;
+
+
+                    //Permite el manejo de las sesiones y los perfiles del sistema 
+
+                    if (data.Count() > 0)
+                    {
+                        Session["Gerencia"] = data.FirstOrDefault().Usuario;
+
+                        //Para los datos del perfil del tipo perfil 1
+                        Session["Cedula"] = data.FirstOrDefault().Cedula;
+                        Session["Nombre"] = data.FirstOrDefault().Nombre_Empleado;
+
+
+                        //Cookie que permite manejar el usuario en sesión, en lugar de usar Session
+                        // FormsAuthentication.SetAuthCookie(pEmpleado.Usuario, false);
+                        return RedirectToAction("Index", "Home");
+
+                    }
+                    else if (data.Count() > 0)
+                    {
+
+                        Session["RH"] = data.FirstOrDefault().Usuario;
+
+                        //Para los datos del perfil del tipo perfil 2
+                        Session["Cedula"] = data.FirstOrDefault().Cedula;
+                        Session["Nombre"] = data.FirstOrDefault().Nombre_Empleado;
+
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else if (data3.Count() > 0)//&& pEmpleado.Perfil.Equals(3))
+                    {
+
+                        Session["Empleado"] = data.FirstOrDefault().Usuario;
+
+                        //Para los datos del perfil del tipo perfil 3
+                        Session["Cedula"] = data.FirstOrDefault().Cedula;
+                        Session["Nombre"] = data.FirstOrDefault().Nombre_Empleado;
+
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+
+                        return RedirectToAction("Registro_Empleados");
+                    }
+
+
+                }
+            }
+            return View();
+        }
+
+
+
+
 
 
     }
--- SoportePatitos/Controllers/RecursosHumanosController.cs
+++ SoportePatitos/Controllers/RecursosHumanosController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SoportePatitosBD.Interface;
+using SoportePatitosBD.Modelo;
+using SoportePatitosBD.Repositorios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,193 @@ namespace SoportePatitos.Controllers
 {
     public class RecursosHumanosController : Controller
     {
-        // GET: RecursosHumanos
-        public ActionResult Index()
+
+        private readonly IGestorEmpleado _oGestorEmpleado;
+        private readonly IGestorEvaluacion _oGestorEvaluacion;
+
+
+
+        //Constructor de el empleado
+        public RecursosHumanosController()
         {
+            _oGestorEmpleado = new GestorEmpleado();
+            _oGestorEvaluacion = new GestorEvaluacion();
+        }
+
+
+
+        // GET: RecursosHumanos
+        //Accion que permite mostrar el formulario para que el empleado se registre
+        public ActionResult Registro_Empleados()
+        {
+
+            //Se inicializan las listas que se usaran más adelante
+            List<Models.ViewModels.Departamento> lst = null;
+            List<Models.ViewModels.Puesto> lst2 = null;
+            List<Models.ViewModels.Perfil> lst3 = null;
+            List<Models.ViewModels.Horario> lst4 = null;
+
+            using (SoportePatitosEntities ContextoBD = new SoportePatitosEntities())
+            {
+                //Permite mostrar un dropdownlist con los departamentos almacenados en la base de datos
+                lst =
+                (from d in ContextoBD.Departamento
+                 select new Models.ViewModels.Departamento
+                 {
+                     ID_departamento = d.ID_departamento,
+                     Descripcion = d.Descripcion
+                 }).ToList();
+
+                List<SelectListItem> items = lst.ConvertAll(d =>
+                {
+
+                    return new SelectListItem
+                    {
+
+                        Text = d.Descripcion.ToString(),
+                        Value = d.ID_departamento.ToString(),
+                        Selected = false
+                    };
+
+                });
+
+
+                //Permite mostrar un dropdownlist con los puestos almacenados en la base de datos
+                lst2 =
+                (from d in ContextoBD.Puesto
+                 select new Models.ViewModels.Puesto
+                 {
+                     ID_puesto = d.ID_puesto,
+                     Descripcion = d.Descripcion
+                 }).ToList();
+
+                List<SelectListItem> items2 = lst2.ConvertAll(d =>
+                {
+
+                    return new SelectListItem
+                    {
+
+                        Text = d.Descripcion.ToString(),
+                        Value = d.ID_puesto.ToString(),
+                        Selected = false
+                    };
+
+                });
+
+
+                //Permite mostrar un dropdownlist con los perfiles almacenados en la base de datos
+                lst3 =
+                (from d in ContextoBD.Perfil
+                 select new Models.ViewModels.Perfil
+                 {
+                     ID_perfil = d.ID_perfil,
+                     Descripcion = d.Descripcion
+                 }).ToList();
+
+                List<SelectListItem> items3 = lst3.ConvertAll(d =>
+                {
+
+                    return new SelectListItem
+                    {
+
+                        Text = d.Descripcion.ToString(),
+                        Value = d.ID_perfil.ToString(),
+                        Selected = false
+                    };
+
+                });
+
+                //Permite mostrar un dropdownlist con los horarios almacenados en la base de datos
+                lst4 =
+                (from d in ContextoBD.Horario
+                 select new Models.ViewModels.Horario
+                 {
+                     ID_horario = d.ID_horario,
+                     Descripcion = d.Descripcion
+                 }).ToList();
+
+                List<SelectListItem> items4 = lst4.ConvertAll(d =>
+                {
+                    return new SelectListItem
+                    {
+
+                        Text = d.Descripcion.ToString(),
+                        Value = d.ID_horario.ToString(),
+                        Selected = false
+                    };
+
+                });
+                ViewData["Departamento"] = items;
+                ViewData["Puesto"] = items2;
+                ViewData["Perfil"] = items3;
+                ViewData["Horario"] = items4;
+                return View();
+            }
+        }
+
+
+
+
+        //Accion que permite Registrar un empleado en la base de datos
+        public ActionResult EnviarEmpleado(Empleado pEmpleado)
+        {
+            int registros = _oGestorEmpleado.CrearEmpleado(pEmpleado);
+            return RedirectToAction("Registro_Empleados");
+        }
+
+
+
+
+
+        //Accion que muestra la pantalla de para evaluar a un empleado
+        public ActionResult Evaluacion()
+        {
+
             return View();
         }
+
+
+        //[Authorize] 
+        //Permite manejar a que páginas esta autorizada el usuario y cuales no
+        //Accion envia la evaluación realizada a un empleado
+        public ActionResult EnviarEvaluacion(Evaluacion pEvaluacion)
+        {
+            int registros = _oGestorEvaluacion.CrearEvaluacion(pEvaluacion);
+            return RedirectToAction("Evaluacion");
+        }
+
+
+        //Accion que muestra la pantalla con el reporte de la evaluacion
+        public ActionResult ListadoEmpleados()
+        {
+            IEnumerable<Empleado> empleados = _oGestorEmpleado.ListadoEmpleados();
+            return View(empleados);
+        }
+
+
+
+        //Accion que muestra la pantalla con el reporte de la evaluacion
+        public ActionResult ReporteEmpleado(int Cedula)
+        {
+            // IEnumerable<Evaluacion> evaluacion = _oGestorEvaluacion.ListadoEvaluacion();
+            //  return View(evaluacion);
+
+            //  Evaluacion obj = _oGestorEvaluacion.ListadoEvaluacion().Where(x => x.Cedula == Cedula).FirstOrDefault();
+            //   return View(obj);
+
+            // SoportePatitosBD.Modelo.spEvaluacionEmpleado_Result;
+
+            //ContextoBD.spEvaluacionEmpleado()
+
+            using (SoportePatitosEntities ContextoBD = new SoportePatitosEntities())
+            {
+                Evaluacion obj = _oGestorEvaluacion.ListadoEvaluacion().Where(x => x.Cedula == Cedula).FirstOrDefault();
+                return View(obj);
+            }
+
+
+        }
+
+
     }
 }

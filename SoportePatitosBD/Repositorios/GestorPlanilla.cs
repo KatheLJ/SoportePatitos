31f287fd1cdd728677b2d9fd687fe1d0c4776abb
KatheLJ <katheloajar257@gmail.com>
2023-03-24 20:14:03 -0600

    Cambios

---
+++ SoportePatitosBD/Repositorios/GestorPlanilla.cs
@@ -0,0 +1,29 @@
+﻿using SoportePatitosBD.Interface;
+using SoportePatitosBD.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoportePatitosBD.Repositorios
+{
+    internal class GestorPlanilla : IGestorPlanilla
+    {
+
+        int IGestorPlanilla.ParamPlanilla()
+        {
+
+            int n = 0;
+            using (SoportePatitosEntities ContextoBD = new SoportePatitosEntities())
+            {
+                ContextoBD.spPLanillla();
+                n = ContextoBD.SaveChanges();
+            }
+            return n;
+
+        }
+
+
+    }
+}

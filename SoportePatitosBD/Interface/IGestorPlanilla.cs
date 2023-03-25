d620e194684ad67f6534649f362dfedb0f6e87d3
KatheLJ <katheloajar257@gmail.com>
2023-03-24 20:36:27 -0600

    archivos para planilla

--- SoportePatitosBD/Interface/IGestorPlanilla.cs
+++ SoportePatitosBD/Interface/IGestorPlanilla.cs
@@ -10,6 +10,22 @@ namespace SoportePatitosBD.Interface
     public interface IGestorPlanilla
     {
         //Permite crear la planilla
-       // int CrearPlanilla(Planilla pPlanilla);
+        // int CrearPlanilla(Planilla pPlanilla);
+
+        //Permite traer los parametros para la planill
+        int ParamPlanilla();
+
+        //Permite realizar la deducción de ausencias
+        int DeducAusencias(int salarioBase, int cantidadDiasAusentes);
+
+
+        //Permite realizar las deducciones fiscales
+        int DeducRenta();
+
+        //Permite realizar las deducciones de la CCSS
+        int DeducSeguro();
+
+        //Permite calcular el salario final
+        int CalSalarioFinal(int DeducAusencias, int DeducRenta, int DeducSeguro);
     }
 }
--- SoportePatitosBD/Repositorios/GestorPlanilla.cs
+++ SoportePatitosBD/Repositorios/GestorPlanilla.cs
@@ -8,16 +8,49 @@ using System.Threading.Tasks;
 
 namespace SoportePatitosBD.Repositorios
 {
-    internal class GestorPlanilla : IGestorPlanilla
+    public class GestorPlanilla : IGestorPlanilla
     {
 
+        //Permite realizar la deducción de ausencias
+        int IGestorPlanilla.DeducAusencias(int salarioBase, int cantidadDiasAusentes)
+        {
+            int deducciones = salarioBase * cantidadDiasAusentes / 30;
+            return deducciones;
+        }
+
+
+
+        //Permite realizar las deducciones fiscales
+        int IGestorPlanilla.DeducRenta()
+        {
+            return 0;
+        }
+
+
+        //Permite realizar las deducciones de la CCSS
+        int IGestorPlanilla.DeducSeguro()
+        {
+            return 0;
+        }
+
+
+        //Permite calcular el salario final
+        int IGestorPlanilla.CalSalarioFinal(int DeducAusencias, int DeducRenta,int DeducSeguro)
+        {
+            int salario = 0;
+            salario = salario - DeducAusencias - DeducRenta - DeducSeguro;
+            return salario;
+        }
+
+
+        //Trae parametros de otras tablas, requeridos para la planilla
         int IGestorPlanilla.ParamPlanilla()
         {
 
             int n = 0;
             using (SoportePatitosEntities ContextoBD = new SoportePatitosEntities())
             {
-                ContextoBD.spPLanillla();
+                //ContextoBD.spPLanillla();
                 n = ContextoBD.SaveChanges();
             }
             return n;
